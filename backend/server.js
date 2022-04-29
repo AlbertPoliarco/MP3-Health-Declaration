b416e83d2ac0e5d8f4caa36f93e1b1d841d4ac27
Nik Poliarco <nikpoliarco@Niks-MacBook-Pro.local>
2022-04-28 23:22:06 -0500

    create .env and connection mongoDB to node.js

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,6 @@
 const express = require("express")
 const cors = require("cors")
-const moongoose = require("mongoose")
+const mongoose = require("mongoose")
 
 require("dotenv").config()
 
@@ -10,6 +10,18 @@ const port = process.env.PORT || 4000
 // Middleware
 app.use(cors())
 app.use(express.json())
+
+// MongoDB connection
+const uri = process.env.ATLAS_URI
+mongoose.connect(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+})
+
+const connection = mongoose.connection
+connection.once("open", () => {
+  console.log("MongoDB database connection is established.")
+})
 
 app.listen(port, () => {
   console.log(`Server is running in port: ${port}`)
