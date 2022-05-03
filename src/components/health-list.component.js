8519d23345718102455a9ac2dfad0ed5465b89f5
Nik Poliarco <nikpoliarco@Niks-MacBook-Pro.local>
2022-05-02 22:18:38 -0500

    healt-list setup

--- src/components/health-list.component.js
+++ src/components/health-list.component.js
@@ -1,7 +1,50 @@
+import axios from "axios"
 import React, { Component } from "react"
 
 export default class HealthList extends Component {
+  constructor(props) {
+    super(props)
+
+    this.state = { health: [] }
+  }
+
+  componentDidMount() {
+    axios
+      .get("http://localhost:4000/health/")
+      .then((res) => {
+        this.setState = { health: res.data }
+      })
+      .catch((error) => {
+        console.log(error)
+      })
+  }
+
+  deleteHealth(id){
+    axios.delete('http://localhost:4000/health/'+id)
+  }
+  healthDeclarations() {
+    return this.state.health.map(currentHealth => {
+      return <Health health={currentHealth} deleteHealth{this.deleteHealth} key={currentHealth._id} />
+    })
+  }
+
   render() {
-    return <div className='container'>Health List</div>
+    return (
+      <div className='container'>
+        <h1>Health List</h1>
+        <table className='table table-bordered table-hover rounded-1'>
+          <thead className='bg-secondary'>
+            <tr className='text-light'>
+              <th>Full Name</th>
+              <th>Temperature</th>
+              <th>Email</th>
+              <th>Phone Number</th>
+              <th className='text-center'>Actions</th>
+            </tr>
+          </thead>
+          {/* <tbody>{this.healthDeclarations()}</tbody>÷ */}
+        </table>
+      </div>
+    )
   }
 }
