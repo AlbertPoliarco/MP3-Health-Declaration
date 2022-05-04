a2c841324905e88613c6c46e6b61291d97d85965
Nik Poliarco <nikpoliarco@Niks-MacBook-Pro.local>
2022-05-03 21:13:07 -0500

    health list works

--- src/components/health-list.component.js
+++ src/components/health-list.component.js
@@ -1,10 +1,40 @@
 import axios from "axios"
 import React, { Component } from "react"
+import { Link } from "react-router-dom"
+
+const Health = (props) => {
+  return (
+    <tr>
+      <td>{props.health.fullname}</td>
+      <td>{props.health.temperature}</td>
+      <td>{props.health.email}</td>
+      <td>{props.health.phonenumber}</td>
+      <td className='text-center'>
+        <Link
+          to={"/edit/" + props.health._id}
+          className='btn btn-sm btn-primary'
+        >
+          Edit
+        </Link>
+        <a
+          href='#'
+          onClick={() => {
+            props.deleteHealth(props.health._id)
+          }}
+          className='btn btn-sm btn-danger'
+        >
+          Delete
+        </a>
+      </td>
+    </tr>
+  )
+}
 
 export default class HealthList extends Component {
   constructor(props) {
     super(props)
 
+    this.deleteHealth = this.deleteHealth.bind(this)
     this.state = { health: [] }
   }
 
@@ -12,19 +42,30 @@ export default class HealthList extends Component {
     axios
       .get("http://localhost:4000/health/")
       .then((res) => {
-        this.setState = { health: res.data }
+        this.setState({ health: res.data })
       })
       .catch((error) => {
         console.log(error)
       })
   }
 
-  deleteHealth(id){
-    axios.delete('http://localhost:4000/health/'+id)
+  deleteHealth(id) {
+    axios
+      .delete("http://localhost:4000/health/" + id)
+      .then((res) => console.log(res.data))
+    this.setState({
+      health: this.state.health.filter((el) => el._id !== id),
+    })
   }
   healthDeclarations() {
-    return this.state.health.map(currentHealth => {
-      return <Health health={currentHealth} deleteHealth{this.deleteHealth} key={currentHealth._id} />
+    return this.state.health.map((currentHealth) => {
+      return (
+        <Health
+          health={currentHealth}
+          deleteHealth={this.deleteHealth}
+          key={currentHealth._id}
+        />
+      )
     })
   }
 
@@ -42,7 +83,7 @@ export default class HealthList extends Component {
               <th className='text-center'>Actions</th>
             </tr>
           </thead>
-          {/* <tbody>{this.healthDeclarations()}</tbody>÷ */}
+          <tbody>{this.healthDeclarations()}</tbody>
         </table>
       </div>
     )
